b8be5b272b09e4a95e37135ba755d58e5b2171df
JINNIELABORE <elaborejinni@gmail.com>
2024-11-07 11:49:02 -0300

    login administrador - marcação

---
+++ src/Eventos/EventoLogin.js
@@ -0,0 +1,46 @@
+import styles from '../Css/footer.module.css';
+
+
+function EventoLogin() {
+    // async function meuEvento() {
+    //     const emailCampo = document.getElementById('email').value;
+    //     const senhaCampo = document.getElementById('senha').value;
+
+    //     let logado = false;
+
+    //     // Validar se os campos estão preenchidos
+    //     if (emailCampo === '' || senhaCampo === '') {
+    //         alert('Dados necessários não preenchidos!');
+    //     } else {
+    //         try {
+    //             const listaAdministradores = await getAdministradores.json(); // pega os adm da api
+
+    //             console.log(listaAdministradores);
+
+    //             if (listaAdministradores.administradores) {
+    //                 listaAdministradores.administradores.forEach((usuario) => {
+    //                     if (emailCampo === usuario.email_admin && senhaCampo === usuario.senha_admin) {
+    //                         logado = true;
+    //                         console.log('Administrador logado com sucesso');
+    //                     }
+    //                 });
+    //             }
+    //             if (!logado) {
+    //                 alert('Login inválido. Usuário não encontrado ou dados incorretos.');
+    //             }
+
+    //         } catch (error) {
+    //             console.error('Erro ao consultar os administradores:', error);
+    //             alert('Erro na validação do login. Tente novamente mais tarde.');
+    //         }
+    //     }
+    // }
+
+    return (
+        <div>
+            <button className={styles.button} onClick={EventoLogin}>Continuar</button>
+        </div>
+    );
+}
+
+export default EventoLogin;
